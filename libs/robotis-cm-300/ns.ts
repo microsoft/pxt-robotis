66a824c2307513ca92d3cade58daa7c0850e4cd4
Kei <kkw@robotis.com>
2020-03-04 18:55:57 +0900

    Add cm300 board package.

---
+++ libs/robotis-cm-300/ns.ts
@@ -0,0 +1,19 @@
+//% color="#d65cd6"
+namespace input {
+}
+
+//% color="#EF2D56" advanced=false
+namespace pins {
+
+}
+
+
+//% color="#006E90"
+namespace serial {
+
+}
+
+//% color="#40bf4a"
+namespace loops {
+
+}
