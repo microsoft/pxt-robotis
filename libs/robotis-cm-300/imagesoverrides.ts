77c6e079b689cfee734e721be8477ae13075a858
sweetfood-dev_company <kjsii123@hanmail.net>
2020-03-27 15:36:31 +0900

    removed category->images, removed extensions->coor-sensor, infrared, lightsensor, mixer, jacdac-games

---
+++ libs/robotis-cm-300/imagesoverrides.ts
@@ -0,0 +1,10 @@
+namespace images {
+    //% fixedInstance jres blockIdentity=icon.__imagePicker
+export const informationBackward = image.ofBuffer(hex``);
+//% fixedInstance jres blockIdentity=icon.__imagePicker
+export const informationForward = image.ofBuffer(hex``);
+//% fixedInstance jres blockIdentity=icon.__imagePicker
+export const informationLeft = image.ofBuffer(hex``);
+//% fixedInstance jres blockIdentity=icon.__imagePicker
+export const informationRight = image.ofBuffer(hex``);
+}
